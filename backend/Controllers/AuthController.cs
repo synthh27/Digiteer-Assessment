aaa506cce4de7ecc09a3c68da2c1377b441e4ee6
synthh <sobrepenacarliannoel@gmail.com>
2026-01-18 13:49:05 +0800

    CREATED Auth Contoller and ADDED Register User and Get User by Id

--- backend/Controllers/AuthController.cs
+++ backend/Controllers/AuthController.cs
@@ -1,6 +1,14 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
 using TaskManager.Data;
+using TaskManager.DTOs;
+using TaskManager.Helpers;
+using TaskManager.Models;
+using TaskManager.Services;
+using static TaskManager.DTOs.DTOs;
 
 namespace task_manager_api.Controllers
 {
@@ -9,10 +17,58 @@ namespace task_manager_api.Controllers
     public class AuthController : ControllerBase
     {
         private readonly ApplicationDbContext _context;
+        private readonly JwtService _jwtService;
 
-        public AuthController(ApplicationDbContext context)
+        public AuthController(ApplicationDbContext context, JwtService jwtService)
         {
             _context = context;
+            _jwtService = jwtService;
+        }
+
+        [HttpGet("{id}")]
+        private async Task<IActionResult> Get(int id)
+        {
+            // GETS USER
+            var user = await _context.Users.FindAsync(id);
+
+            //RETURN 404 IF USER DO NOT EXIST
+            if (user is null) return NotFound("User do not exist.");
+
+            return Ok(user);
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> register(AuthRequest request)
+        {
+            // CHECK IF EMAIL EXIST
+            var isExistingUser = await _context.Users.AnyAsync(u => u.Email == request.Email);
+
+            // RETURN 409 IF EMAIL ALREADY USED.
+            if (isExistingUser) return Conflict("Email already used");
+
+            // CREATES NEW USER
+            var newUser = new User
+            {
+                Email = request.Email,
+                PasswordHash = Utility.HashPasword(request.Password)
+            };
+
+            // CREATE NEW JWT TOKEN
+            var token = _jwtService.GenerateToken(newUser);
+
+            // SAVES USER TO DATABASE
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            // RETURN 201 CREATION SUCCESS RESPONSE
+            return CreatedAtAction(
+                nameof(Get),
+                new{ Id = newUser.Id },
+                new AuthResponse(
+                    "User registered successfully.",
+                    newUser.Id, newUser.Email, token
+                )
+            );
         }
     }
 }
