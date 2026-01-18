46fb14540668c5c85252c9ef1bafbcff59473406
synthh <sobrepenacarliannoel@gmail.com>
2026-01-18 13:02:25 +0800

    CREATED Auth Controller

---
+++ backend/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Data;
+
+namespace task_manager_api.Controllers
+{
+    [Route("api/auth")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
