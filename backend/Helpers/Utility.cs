061d128c156277df4111076a871259a3bdf665a4
synthh <sobrepenacarliannoel@gmail.com>
2026-01-18 13:07:38 +0800

    CREATED Utility Helper for Hashing and Verifying Passwords

---
+++ backend/Helpers/Utility.cs
@@ -0,0 +1,16 @@
+﻿
+namespace TaskManager.Helpers
+{
+    static public class Utility
+    {
+        public static string HashPasword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public static bool HashPassword(string password, string hashedPassword)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+        }
+    }
+}
