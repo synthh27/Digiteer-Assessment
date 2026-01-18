000ed39d196f69d99a25806d7237db88c32d071b
synthh <sobrepenacarliannoel@gmail.com>
2026-01-18 13:21:38 +0800

    ADDED DTOs for Auth Request and Reponse

---
+++ backend/DTOs/DTOs.cs
@@ -0,0 +1,19 @@
+﻿namespace task_manager_api.DTOs
+{
+    public class DTOs
+    {
+        public record AuthRequest
+        (
+            string Email,
+            string Password
+        );
+
+        public record AuthResponse
+        (
+            string? Message,
+            int userId,
+            string Email,
+            string Token
+        );
+    }
+}
