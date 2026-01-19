a06e9b543425a22b2b5daa7a31bafc043b13ad60
synthh <sobrepenacarliannoel@gmail.com>
2026-01-19 21:05:43 +0800

    FIXED 'server' typo

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  serve: {
+  server: {
     proxy: {
       '/api': "https://localhost:7144"
     }
